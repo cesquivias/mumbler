be2f972f9ac0c3a589f2fff1cef7d78964cef284
Cristian Esquivias <cristian.esquivias@gmail.com>
2014-10-01 13:01:32 -0700

    Added * and / builtins.

--- src/java/truffler/env/BaseEnvironment.java
+++ src/java/truffler/env/BaseEnvironment.java
@@ -49,10 +49,54 @@ public class BaseEnvironment {
             }
         };
 
+    private static final Fn MULT = new BuiltinFn("MULT") {
+            @Override
+            public Object apply(Object... args) {
+                long product = 1;
+                for (Object arg : args) {
+                    product *= (Long) arg;
+                }
+                return product;
+            }
+        };
+
+    private static final Fn DIV = new BuiltinFn("DIV") {
+            @Override
+            public Object apply(Object... args) {
+                if (args.length == 1) {
+                    return 1 / (Long) args[0];
+                }
+                long quotient = (Long) args[0];
+                for (int i=1; i<args.length; i++) {
+                    quotient /= (Long) args[i];
+                }
+                return quotient;
+            }
+        };
+
+    private static final Fn EQUALS = new BuiltinFn("EQUALS") {
+            @Override
+            public Object apply(Object... args) {
+                Long last = (Long) args[0];
+                for (Object arg : args) {
+                    Long current = (Long) arg;
+                    if (!last.equals(current)) {
+                        return false;
+                    } else {
+                        last = current;
+                    }
+                }
+                return true;
+            }
+        };
+
     public static Environment getBaseEnvironment() {
         Environment env = new Environment();
         env.putValue(new SymbolForm("+"), PLUS);
         env.putValue(new SymbolForm("-"), MINUS);
+        env.putValue(new SymbolForm("*"), MULT);
+        env.putValue(new SymbolForm("/"), DIV);
+        env.putValue(new SymbolForm("="), EQUALS);
         return env;
     }
 }
