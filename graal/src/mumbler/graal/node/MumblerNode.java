07f98b24af5ec81ba3c3addf699df987acfad366
Cristian Esquivias <cristian.esquivias@gmail.com>
2014-12-03 12:42:13 -0800

    Added execute* methods to MumblerNode.

--- graal/src/mumbler/graal/node/MumblerNode.java
+++ graal/src/mumbler/graal/node/MumblerNode.java
@@ -1,10 +1,48 @@
 package mumbler.graal.node;
 
-import mumbler.graal.Evaluatable;
+import mumbler.graal.MumblerTypesGen;
+import mumbler.graal.type.MumblerFunction;
+import mumbler.graal.type.MumblerList;
+import mumbler.graal.type.MumblerSymbol;
 
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeInfo;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
 
 @NodeInfo(language = "Mumbler Language", description = "The abstract base node for all expressions")
-public abstract class MumblerNode extends Node implements Evaluatable  {
+public abstract class MumblerNode extends Node {
+
+    public abstract Object execute(VirtualFrame virtualFrame);
+
+    public long executeLong(VirtualFrame virtualFrame)
+            throws UnexpectedResultException {
+        return MumblerTypesGen.MUMBLERTYPES.expectLong(
+                this.execute(virtualFrame));
+    }
+
+    public boolean executeBoolean(VirtualFrame virtualFrame)
+            throws UnexpectedResultException {
+        return MumblerTypesGen.MUMBLERTYPES.expectBoolean(
+                this.execute(virtualFrame));
+    }
+
+    public MumblerSymbol executeMumblerSymbol(VirtualFrame virtualFrame)
+            throws UnexpectedResultException {
+        return MumblerTypesGen.MUMBLERTYPES.expectMumblerSymbol(
+                this.execute(virtualFrame));
+    }
+
+    public MumblerFunction executeMumblerFunction(VirtualFrame virtualFrame)
+            throws UnexpectedResultException {
+        return MumblerTypesGen.MUMBLERTYPES.expectMumblerFunction(
+                this.execute(virtualFrame));
+
+    }
+
+    public MumblerList<?> executeMumblerList(VirtualFrame virtualFrame)
+            throws UnexpectedResultException {
+        return MumblerTypesGen.MUMBLERTYPES.expectMumblerList(
+                this.execute(virtualFrame));
+    }
 }
