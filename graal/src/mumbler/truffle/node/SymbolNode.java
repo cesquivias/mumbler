5c448ad77a5bc7b9bad7c59247f630d4d358b3f5
Grashalm <christian.humer@gmail.com>
2014-12-18 16:48:45 +0100

    Basic version of local variable lookup speculation.

--- graal/src/mumbler/truffle/node/SymbolNode.java
+++ graal/src/mumbler/truffle/node/SymbolNode.java
@@ -1,5 +1,7 @@
 package mumbler.truffle.node;
 
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.dsl.NodeField;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.Frame;
@@ -7,25 +9,51 @@ import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
 
 @NodeField(name = "slot", type = FrameSlot.class)
 public abstract class SymbolNode extends MumblerNode {
+
     public abstract FrameSlot getSlot();
+
+    @CompilationFinal
+    FrameSlot resolvedSlot;
+    @CompilationFinal
+    int lookupDepth;
 
     public static interface FrameGet<T> {
         public T get(Frame frame, FrameSlot slot) throws FrameSlotTypeException;
     }
 
+    @ExplodeLoop
     public <T> T readUpStack(FrameGet<T> getter, Frame frame)
             throws FrameSlotTypeException {
+        if (this.resolvedSlot == null) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            this.resolveSlot(getter, frame);
+        }
+
+        Frame lookupFrame = frame;
+        for (int i = 0; i < this.lookupDepth; i++) {
+            lookupFrame = this.getLexicalScope(lookupFrame);
+        }
+        return getter.get(lookupFrame, this.resolvedSlot);
+    }
+
+    private <T> void resolveSlot(FrameGet<T> getter, Frame frame)
+            throws FrameSlotTypeException {
+
         FrameSlot slot = this.getSlot();
+        int depth = 0;
         Object identifier = slot.getIdentifier();
         T value = getter.get(frame, slot);
         while (value == null) {
+            depth++;
             frame = this.getLexicalScope(frame);
             if (frame == null) {
-                throw new RuntimeException("Unknown variable: " +
-                        this.getSlot().getIdentifier());
+                CompilerDirectives.transferToInterpreterAndInvalidate();
+                throw new RuntimeException("Unknown variable: "
+                        + this.getSlot().getIdentifier());
             }
             FrameDescriptor desc = frame.getFrameDescriptor();
             slot = desc.findFrameSlot(identifier);
@@ -33,7 +61,8 @@ public abstract class SymbolNode extends MumblerNode {
                 value = getter.get(frame, slot);
             }
         }
-        return value;
+        this.lookupDepth = depth;
+        this.resolvedSlot = slot;
     }
 
     @Specialization(rewriteOn = FrameSlotTypeException.class)
@@ -54,7 +83,7 @@ public abstract class SymbolNode extends MumblerNode {
         return this.readUpStack(Frame::getObject, virtualFrame);
     }
 
-    @Specialization(contains = {"readLong", "readBoolean", "readObject"})
+    @Specialization(contains = { "readLong", "readBoolean", "readObject" })
     protected Object read(VirtualFrame virtualFrame) {
         try {
             return this.readUpStack(Frame::getValue, virtualFrame);
