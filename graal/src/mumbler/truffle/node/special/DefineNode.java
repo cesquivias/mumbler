8df119f46614d9a04eff8a74b9432a5ef71dbcb3
Cristian Esquivias <cristian.esquivias@gmail.com>
2015-04-02 23:39:48 -0700

    Fixed graal version to work with latest API changes to Truffle

--- graal/src/mumbler/truffle/node/special/DefineNode.java
+++ graal/src/mumbler/truffle/node/special/DefineNode.java
@@ -14,56 +14,56 @@ import com.oracle.truffle.api.nodes.Node;
 @NodeChild("valueNode")
 @NodeField(name = "slot", type = FrameSlot.class)
 public abstract class DefineNode extends MumblerNode {
-    protected abstract FrameSlot getSlot();
-    protected abstract Node getValueNode();
+	protected abstract FrameSlot getSlot();
+	protected abstract Node getValueNode();
 
-    @Specialization(guards = "isLongKind")
-    protected long writeLong(VirtualFrame virtualFrame, long value) {
-        virtualFrame.setLong(this.getSlot(), value);
-        return value;
-    }
+	@Specialization(guards = "isLongKind()")
+	protected long writeLong(VirtualFrame virtualFrame, long value) {
+		virtualFrame.setLong(this.getSlot(), value);
+		return value;
+	}
 
-    @Specialization(guards = "isBooleanKind")
-    protected boolean writeBoolean(VirtualFrame virtualFrame, boolean value) {
-        virtualFrame.setBoolean(this.getSlot(), value);
-        return value;
-    }
+	@Specialization(guards = "isBooleanKind()")
+	protected boolean writeBoolean(VirtualFrame virtualFrame, boolean value) {
+		virtualFrame.setBoolean(this.getSlot(), value);
+		return value;
+	}
 
-    @Specialization(contains = {"writeLong", "writeBoolean"})
-    protected Object write(VirtualFrame virtualFrame, Object value) {
-        FrameSlot slot = this.getSlot();
-        if (slot.getKind() != FrameSlotKind.Object) {
-            CompilerDirectives.transferToInterpreterAndInvalidate();
-            slot.setKind(FrameSlotKind.Object);
-        }
-        virtualFrame.setObject(slot, value);
-        return value;
-    }
+	@Specialization(contains = {"writeLong", "writeBoolean"})
+	protected Object write(VirtualFrame virtualFrame, Object value) {
+		FrameSlot slot = this.getSlot();
+		if (slot.getKind() != FrameSlotKind.Object) {
+			CompilerDirectives.transferToInterpreterAndInvalidate();
+			slot.setKind(FrameSlotKind.Object);
+		}
+		virtualFrame.setObject(slot, value);
+		return value;
+	}
 
-    protected boolean isLongKind() {
-        return this.isKind(this.getSlot(), FrameSlotKind.Long);
-    }
+	protected boolean isLongKind() {
+		return this.isKind(this.getSlot(), FrameSlotKind.Long);
+	}
 
-    protected boolean isBooleanKind() {
-        return this.isKind(this.getSlot(), FrameSlotKind.Boolean);
-    }
+	protected boolean isBooleanKind() {
+		return this.isKind(this.getSlot(), FrameSlotKind.Boolean);
+	}
 
-    private boolean isKind(FrameSlot slot, FrameSlotKind kind) {
-        if (slot.getKind() == kind) {
-            return true;
-        }
-        if (slot.getKind() == FrameSlotKind.Illegal) {
-            CompilerDirectives.transferToInterpreterAndInvalidate();
-            slot.setKind(kind);
-            return true;
-        }
-        return false;
-    }
+	private boolean isKind(FrameSlot slot, FrameSlotKind kind) {
+		if (slot.getKind() == kind) {
+			return true;
+		}
+		if (slot.getKind() == FrameSlotKind.Illegal) {
+			CompilerDirectives.transferToInterpreterAndInvalidate();
+			slot.setKind(kind);
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public String toString() {
-        return "(define " + this.getSlot().getIdentifier() + " "
-                + this.getValueNode() + ")";
-    }
+	@Override
+	public String toString() {
+		return "(define " + this.getSlot().getIdentifier() + " "
+				+ this.getValueNode() + ")";
+	}
 }
 
--- graal/src/mumbler/truffle/node/special/QuoteNode.java
+++ graal/src/mumbler/truffle/node/special/QuoteNode.java
@@ -11,37 +11,37 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 @NodeChild("literalNode")
 @NodeField(name = "kind", type = QuoteKind.class)
 public abstract class QuoteNode extends MumblerNode {
-    public static enum QuoteKind {
-        LONG,
-        BOOLEAN,
-        STRING,
-        SYMBOL,
-        LIST
-    }
+	public static enum QuoteKind {
+		LONG,
+		BOOLEAN,
+		STRING,
+		SYMBOL,
+		LIST
+	}
 
-    protected abstract QuoteKind getKind();
+	protected abstract QuoteKind getKind();
 
-    @Specialization(guards = "isLongKind")
-    protected long quoteLong(VirtualFrame virtualFrame, long value) {
-        return value;
-    }
+	@Specialization(guards = "isLongKind()")
+	protected long quoteLong(VirtualFrame virtualFrame, long value) {
+		return value;
+	}
 
-    @Specialization(guards = "isBooleanKind")
-    protected boolean quoteBoolean(VirtualFrame virtualFrame, boolean value) {
-        return value;
-    }
+	@Specialization(guards = "isBooleanKind()")
+	protected boolean quoteBoolean(VirtualFrame virtualFrame, boolean value) {
+		return value;
+	}
 
-    @Specialization(contains = {"quoteLong", "quoteBoolean"})
-    protected Object quote(VirtualFrame virtualFrame, Object value) {
-        return value;
-    }
+	@Specialization(contains = {"quoteLong", "quoteBoolean"})
+	protected Object quote(VirtualFrame virtualFrame, Object value) {
+		return value;
+	}
 
-    protected boolean isLongKind() {
-        return this.getKind() == QuoteKind.LONG;
-    }
+	protected boolean isLongKind() {
+		return this.getKind() == QuoteKind.LONG;
+	}
 
-    protected boolean isBooleanKind() {
-        return this.getKind() == QuoteKind.BOOLEAN;
-    }
+	protected boolean isBooleanKind() {
+		return this.getKind() == QuoteKind.BOOLEAN;
+	}
 }
 
